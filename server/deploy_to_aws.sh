a09b31317830bf598103740f21cdb78a0d5c1fcc
Vadims Tihomirovs <vadims.tihomirovs@sony.com>
2018-05-24 11:58:32 +0200

    server deploy command

---
+++ server/deploy_to_aws.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function info()
+{
+    echo -e "\e[00;32m$*\e[00m" >&2 #green
+}
+
+if [ ! -e xelnaga_ami_data ]; then
+    info "file xelnaga_ami_data does not exist. will be created with default options"
+
+    touch xelnaga_ami_data
+    echo AWS_HOST_CERT=xelnaga_ami_key.pem > xelnaga_ami_data
+    echo AWS_HOST_NAME=localhost >> xelnaga_ami_data
+    echo AWS_HOST_USER=ubuntu >> xelnaga_ami_data
+    echo AWS_HOST_PATH=/home/ubuntu >> xelnaga_ami_data
+fi
+
+. xelnaga_ami_data
+
+function copy_to_aws()
+{
+    FILE_NAME=$*
+    scp -i $AWS_HOST_CERT $FILE_NAME $AWS_HOST_USER@$AWS_HOST_NAME:$AWS_HOST_PATH/$FILE_NAME
+}
+
+info "copy file to aws"
+copy_to_aws shp_server
+copy_to_aws service-account.json
+
+#ssh -v -i $AWS_HOST_CERT $AWS_HOST_USER@$AWS_HOST_NAME
