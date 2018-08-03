48b979db80565e40a83c374b2f82d7a6adef0f28
Vadims Tihomirovs <vadims.tihomirovs@sony.com>
2018-08-03 17:03:36 +0200

    ESP32 support added

--- setup.sh
+++ setup.sh
@@ -39,6 +39,7 @@ if [ ! -d $jsonlib ]; then
 else
     info "cpp-json already in place, skipping ..."
 fi
+echo
 
 
 # get openssl
@@ -61,6 +62,7 @@ if [ ! -d $openssl ]; then
 else
     info "openssl already in place, skipping ..."
 fi
+echo
 
 
 # get curl
@@ -87,6 +89,7 @@ if [ ! -d $curl ]; then
 else
     info "curl already in place, skipping ..."
 fi
+echo
 
 # get gtest
 gtest=$external/googletest
@@ -106,10 +109,10 @@ if [ ! -d $gtest ]; then
 else
     info "gtest already in place, skipping ..."
 fi
+echo
 
 
-# check the machine architecture
-tools=$external/tools
+# check the machine architecture and grab cross-compilers
 arch=$(uname --machine)
 if [ $arch == "x86_64" ]; then
     info "you are using x86 ($arch) architecture"
@@ -118,35 +121,80 @@ if [ $arch == "x86_64" ]; then
     echo    # new line
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         echo
-        # get cross-compile tools
-        if [ ! -d $tools ]; then
-            cd $external
 
-            info "clone tools..."
+        # tools for arm64
+        tools_arm=$external/tools_arm
+
+        # get cross-compile tools for arm 64
+        if [ ! -d $tools_arm ]; then
+            mkdir -p $tools_arm
+            cd $tools_arm
+
+            info "clone arm tools..."
             git clone https://github.com/raspberrypi/tools
             if [ "$?" -ne "0" ]; then
-                error "failed to get tools"
+                error "failed to get arm tools"
             fi
 
             info "checkout old but stable version ..."
-            cd $tools
+            cd $tools_arm/tools
             git checkout 3a413ca2b23fd275e8ddcc34f3f9fc3a4dbc723f
             if [ "$?" -ne "0" ]; then
                 error "failed to checkout"
             fi
         else
-            info "tools already in place, skipping ..."
+            info "arm tools already in place, skipping ..."
         fi
 
-        info "create makefile.prefix ..."
+        info "create makefile.prefix_arm ..."
         cd $root
-        echo "PREFIX = $tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-" > ./client/makefile.prefix
-        echo "WIRING_LIB := yes" >> ./client/makefile.prefix
+        echo "PREFIX = $tools_arm/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-" > ./client/makefile.prefix_arm
+        echo "WIRING_LIB := yes" >> ./client/makefile.prefix_arm
+        echo
+
+
+        # tools for esp32
+        tools_esp32=$external/tools_esp
+
+        # get cross-compile tools
+        if [ ! -d $tools_esp32 ]; then
+            cd $external
+
+            wget -c https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
+            if [ "$?" -ne "0" ]; then
+                error "failed to download esp tools"
+            fi
+
+            mkdir -p $tools_esp32
+            cd $tools_esp32
+            tar -xzvf $external/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
+
+            cd $external
+            rm xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
+
+            cd $tools_esp32
+            info "clone esp idf ..."
+            git clone --recursive https://github.com/espressif/esp-idf.git
+            if [ "$?" -ne "0" ]; then
+                error "failed to get esp idf"
+            fi
+
+        else
+            info "esp tools already in place, skipping ..."
+        fi
+
+        info "create makefile.prefix_esp ..."
+        cd $root
+        echo "PREFIX = $tools_esp32/xtensa-esp32-elf/bin/xtensa-esp32-elf-" > ./client/makefile.prefix_esp
+        echo "WIRING_LIB := yes" >> ./client/makefile.prefix_esp
+        echo
+
     else
-        echo "PREFIX = " > ./client/makefile.prefix
+        echo "PREFIX = " > ./client/makefile.prefix_arm
+        echo "PREFIX = " > ./client/makefile.prefix_esp
     fi
 fi
-
+echo
 
 # get wiringPi
 wiringPi=$external/wiringPi
